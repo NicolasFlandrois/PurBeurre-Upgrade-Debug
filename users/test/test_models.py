c5edb9e7006728842774c3213481c35914ba83c6
NicolasFlandrois <n.flandrois9github@gmail.com>
2020-05-06 21:30:08 +0200

    Cleanning code
    Removing commented code, not used

--- users/test/test_models.py
+++ users/test/test_models.py
@@ -15,16 +15,9 @@ class TestModels(TestCase):
             email='boggusmail@boggusmail.net'
         )
 
-        # self.profile_1 = Profile.objects.create(user=self.user_1,
-        #                                         image='profile_default.jpg')
-
     def test_Profile(self):
         """Testing the Profile class' object"""
         self.assertEquals(self.user_1.username, 'testuser')
-        # self.assertEquals(self.user_1.password, '12345')
+        self.assertEquals(self.user_1.password, '12345')
         self.assertEquals(self.user_1.email,
                           'boggusmail@boggusmail.net')
-
-    # def test_Profile_is_saved_on_creation(self):
-    #     """Testing if the overwitten save() methode works, from Profile"""
-    #     self.assertEquals(self.profile_1.image, 'profile_default.jpg')
