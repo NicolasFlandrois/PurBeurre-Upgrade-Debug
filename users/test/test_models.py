2dec1001cc87203be15c0db7ed05430675906bdf
NicolasFlandrois <n.flandrois9github@gmail.com>
2020-05-07 14:55:12 +0200

    Created Unit Tests for users.models.test_profile > New Feat = Newsletter
    ..................................
    ----------------------------------------------------------------------
    Ran 36 tests in 7.607s
    
    OK

--- users/test/test_models.py
+++ users/test/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import check_password
+from users.models import Profile
 
 
 class TestModels(TestCase):
@@ -16,9 +17,23 @@ class TestModels(TestCase):
 
     def test_Profile(self):
         """Testing the Profile class' object"""
-        client = Client()
-        client.login(username='testuser', password='12345')
+
+        # Setup User
+        client_1 = Client()
+        client_1.login(username='testuser', password='12345')
+        # Setup Profile
+        profile_1 = Profile()
+        profile_2 = Profile(image='profile_not_default.jpg', newsletter=True)
+
+        # Testing User
         self.assertEquals(self.user_1.username, 'testuser')
         self.assertTrue(check_password('12345', self.user_1.password))
         self.assertEquals(self.user_1.email,
                           'boggusmail@boggusmail.net')
+
+        # Testing Profile Default @ Creation
+        self.assertEquals(profile_1.image, 'profile_default.jpg')
+        self.assertFalse(profile_1.newsletter)
+        # Testing Profile Non Default
+        self.assertEquals(profile_2.image, 'profile_not_default.jpg')
+        self.assertTrue(profile_2.newsletter)
